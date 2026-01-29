de781d24b474fe41106466d8ee835e8263a3717b
Ali Shouman <alyshouman@hotmail.com>
2026-01-29 20:11:32 +0000

    update code connect file

--- packages/client/src/components/common/MoreOptionsMenu/MoreOptionsMenu.figma.tsx
+++ packages/client/src/components/common/MoreOptionsMenu/MoreOptionsMenu.figma.tsx
@@ -1,12 +1,11 @@
 import figma from '@figma/code-connect';
 import { MoreOptionsMenu, MenuItem } from './MoreOptionsMenu';
+import { Trash2 } from 'lucide-react';
 
 figma.connect(MoreOptionsMenu, 'https://www.figma.com/design/7QW0kJ07DcM36mgQUJ5Dtj/Carton-Case-Management?node-id=1179-62911&m=dev', {
   example: () => (
     <MoreOptionsMenu>
-      <MenuItem>Edit</MenuItem>
-      <MenuItem>Share</MenuItem>
-      <MenuItem destructive>Delete Case</MenuItem>
+        <MenuItem icon={<Trash2 />}>Delete</MenuItem>
     </MoreOptionsMenu>
   ),
 });
