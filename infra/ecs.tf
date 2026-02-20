2d4e5ed28e967168f7de0344a2d5baf26860515f
Jake Moomaw <jake@bitovi.com>
2026-02-20 12:26:17 -0600

    Change CPU architecture from ARM64 to X86_64

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -10,7 +10,7 @@ resource "aws_ecs_task_definition" "main" {
 
   runtime_platform {
     operating_system_family = "LINUX"
-    cpu_architecture        = "ARM64"
+    cpu_architecture        = "X86_64"
   }
 
   container_definitions = jsonencode([
